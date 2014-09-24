0bfa8aa1c33d5f821ae855ca4e76eddba9f88b07
Rich Wareham <rjw57@cam.ac.uk>
2014-09-24 16:27:05 +0100

    docs/conf.py: fix incorrect placement of brackets

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import sys
 import os
 
 # Add the root of this project to the package path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__, '..'))))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import trafficdb
 
