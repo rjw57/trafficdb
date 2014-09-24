1b81f753704d1f69a1deaf524b780589bc49a92f
Rich Wareham <rjw57@cam.ac.uk>
2014-09-24 14:23:32 +0100

    add some stub documentation

---
+++ trafficdb/__init__.py
@@ -0,0 +1,15 @@
+"""
+Traffic Database
+================
+
+.. automodule:: trafficdb.wsgi
+    :members:
+
+"""
+
+__project__ = 'trafficdb'
+__version__ = '0.0.1'
+__author__ = 'Rich Wareham'
+__author_email__ = 'rich.trafficdb@richwareham.com'
+__copyright__ = '2014, ' + __author__
+
