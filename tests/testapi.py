bf759d5dc3fa7e17cfb65e91f4a28ff06571ffa7
Rich Wareham <rjw57@cam.ac.uk>
2014-09-25 20:55:48 +0100

    api: retrieve simple list of links via /api/links
    The exact form of the returned output is still up in the air a little.

--- tests/testapi.py
+++ tests/testapi.py
@@ -1,11 +1,15 @@
+import datetime
 import logging
+try:
+    from urlparse import urljoin
+    from urllib import urlencode
+except ImportError:
+    from urllib.parse import urljoin, urlencode
 
-from mixer.backend.flask import mixer
-
-from trafficdb.models import *
 from trafficdb.wsgi import db
 
-from .util import TestCase, raises_integrity_error
+from .fixtures import create_fake_observations
+from .util import TestCase
 
 log = logging.getLogger(__name__)
 
@@ -16,3 +20,55 @@ class TestApiRoot(TestCase):
         response = self.client.get(API_PREFIX + '/')
         self.assertIsNot(response.json, None)
         self.assertEquals(response.json, dict(version=1))
+
+class TestSimpleQueries(TestCase):
+    @classmethod
+    def create_fixtures(cls):
+        start_date = datetime.datetime(2013, 9, 10)
+
+        # NOTE: total links is 104
+        create_fake_observations(
+            link_count=4, start=start_date, duration=3*24*60)
+        create_fake_observations(
+            link_count=100, start=start_date, duration=60)
+
+    def test_link_count(self):
+        from trafficdb.models import Link
+        n_links = db.session.query(Link.id).count()
+        log.info('Total links: {0}'.format(n_links))
+        self.assertEqual(n_links, 104)
+
+    def test_all_links(self):
+        log.info('Querying all links')
+        query = None
+        n_links = 0
+        while True:
+            url = API_PREFIX + '/links'
+            if query is not None:
+                url += '?' + urlencode(query)
+
+            log.info('GET {0}'.format(url))
+            response = self.client.get(url)
+            self.assertIsNot(response.json, None)
+
+            page = response.json['page']
+            links = response.json['data']
+            log.info('Got {0} links'.format(len(links)))
+            log.info('Page structure: {0}'.format(page))
+
+            n_links += len(links)
+
+            if query is not None:
+                self.assertTrue(page['first'] >= query['from'])
+
+            self.assertTrue(page['last'] >= page['first'])
+            self.assertTrue(page['count'] == len(links))
+
+            if not page['more']:
+                break
+
+            query = query or {}
+            query['from'] = page['last'] + 1
+
+        log.info('Got information on {0} link(s)'.format(n_links))
+        self.assertEqual(n_links, 104)
--- trafficdb/blueprint/api.py
+++ trafficdb/blueprint/api.py
@@ -1,6 +1,10 @@
 """API to access and modify traffic data records."""
 
 from flask import *
+from sqlalchemy import func
+
+from trafficdb.models import *
+from trafficdb.wsgi import db
 
 __all__ = ['api']
 
@@ -10,6 +14,37 @@ api = Blueprint('api', __name__)
 # alias api as "app" for use below
 app = api
 
+# Maximum number of results to return
+PAGE_LIMIT = 20
+
 @app.route('/')
 def index():
     return jsonify(dict(version=1))
+
+@app.route('/links')
+def links():
+    # Query link objects
+    links_q = db.session.query(Link.id, func.ST_AsText(Link.geom)).order_by(Link.id).\
+            filter(Link.id >= request.args.get('from', 0)).\
+            limit(PAGE_LIMIT+1)
+
+    def row_to_dict(row):
+        return dict(id=row[0], geom=row[1])
+
+    links = list(row_to_dict(l) for l in links_q)
+
+    # How many links to return and do we still have more?
+    count = min(PAGE_LIMIT, len(links))
+    more = (len(links) == PAGE_LIMIT+1)
+
+    # Limit size of output
+    links = links[:PAGE_LIMIT]
+
+    # Form response
+    page = dict(
+        first = links[0]['id'],
+        last = links[-1]['id'],
+        count = count, more=more
+    )
+    response = dict(data=links, page=page)
+    return jsonify(response)
