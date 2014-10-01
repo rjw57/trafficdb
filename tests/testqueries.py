ded24fd17203518b082b1507b22aa8b8b15ac15d
Rich Wareham <rjw57@cam.ac.uk>
2014-10-01 16:01:51 +0100

    tests: missed one change from 8b8338552c4b18f43627dddc634c7e1f84d8515e
    Add missing modification to tests from
    8b8338552c4b18f43627dddc634c7e1f84d8515e for making query test not
    sensitive to order of results.

--- tests/testqueries.py
+++ tests/testqueries.py
@@ -66,13 +66,16 @@ class TestLinkAlias(TestCase):
         q = resolve_link_aliases(db.session, alias_names, self.tmp_table)
         self.assertEqual(q.count(), len(alias_names))
 
-        for alias, row in zip(alias_names, q):
-            log.info('Resolution of {0} is {1}'.format(alias, row))
-            self.assertEqual(alias, row[0])
-            if alias.startswith('_invalid'):
-                self.assertIsNone(row[1])
+        alias_name_set = set(alias_names)
+        for name, id, uuid in q:
+            log.info('Resolution of {0} is {1}'.format(name, (id,uuid)))
+            self.assertIn(name, alias_name_set)
+            if name.startswith('_invalid'):
+                self.assertIsNone(id)
+                self.assertIsNone(uuid)
             else:
-                self.assertIsNotNone(row[1])
+                self.assertIsNotNone(id)
+                self.assertIsNotNone(uuid)
 
     def test_empty_link_alias_query(self):
         alias_names = []
